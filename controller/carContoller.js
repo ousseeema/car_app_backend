068748f4845f56399da63ce8bddfaa6dceb7ee4b
ousseeema <oussama.ferchichi01@gmail.com>
2024-01-22 21:06:56 +0100

    get single car from database

--- controller/carContoller.js
+++ controller/carContoller.js
@@ -21,10 +21,41 @@ exports.getAllCars = asynchandler(async(req, res, next)=>{
 
 });
 
+
+// gettting a single car frpm data base 
+// private
 exports.getcar = asynchandler(async(req, res, next)=>{
 
+const id= req.params.id;
 
+
+if (!id) {
+  return res.status(400).send({
+    message: 'No car id provided',
+    success: false
+  })
+}
+
+
+   const car = await carmodel.findById(id);
+   if(!car){
+    return res.status(404).send({
+      message : 'No car found',
+      success : false
+    })
+   }
+
+
+   return res.status(200).send({
+    message: 'Car found',
+    data : car,
+    success: true 
+   })
 
 
   
 })
+
+
+// uploaing a car
+//private 
